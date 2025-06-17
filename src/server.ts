8bac051f8010270f1103460e75db7bdfefa67350
coder7475 <robiulhossain7475@gmail.com>
2025-06-17 18:20:10 +0600

    feat: add connecting to mongodb using mongoose with graceful disconnection

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
 import app from "@/app";
 import { env } from "@/configs/envConfig";
+import mongoose from "mongoose";
 
 const server = app.listen(env.PORT, () => {
   console.log(
     `🚀 Server (${env.NODE_ENV}) running at http://${env.HOST}:${env.PORT}`,
   );
+
+  // connect to database
+  connectToMongoDB();
 });
 
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(env.MONGODB_URI);
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1); // Exit if DB connection fails
+  }
+}
+
 // Graceful shutdown handler
-const onCloseSignal = () => {
+const onCloseSignal = async () => {
   console.log("🛑 SIGINT/SIGTERM received, shutting down gracefully...");
 
+  try {
+    await mongoose.disconnect();
+    console.log("✅ Disconnected from MongoDB");
+  } catch (err) {
+    console.error("⚠️ Error disconnecting from MongoDB:", err);
+  }
   server.close(() => {
     console.log("✅ Server closed");
     process.exit(0);
