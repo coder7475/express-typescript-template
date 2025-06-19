f4ce8815a1257e035e9a1724056f59f055b78896
coder7475 <robiulhossain7475@gmail.com>
2025-06-19 23:06:46 +0600

    feat: add linting and formating using biome.js

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,14 @@
 import app from "@/app";
 import { env } from "@/configs/envConfig";
-import mongoose from "mongoose";
 
 const server = app.listen(env.PORT, () => {
 	console.log(`🚀 Server (${env.NODE_ENV}) running at http://${env.HOST}:${env.PORT}`);
 });
 
 // Graceful shutdown handler
-const onCloseSignal = async () => {
+const onCloseSignal = () => {
 	console.log("🛑 SIGINT/SIGTERM received, shutting down gracefully...");
 
-	try {
-		await mongoose.disconnect();
-		console.log("✅ Disconnected from MongoDB");
-	} catch (err) {
-		console.error("! Error disconnecting from MongoDB:", err);
-	}
 	server.close(() => {
 		console.log("✅ Server closed");
 		process.exit(0);
