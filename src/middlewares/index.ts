16589d26799d3e4ca0bd89a3b1ed879110c959cb
coder7475 <robiulhossain7475@gmail.com>
2025-06-24 22:21:32 +0600

    fix: build dts type

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,13 @@
+import type { ErrorRequestHandler, RequestHandler } from "express";
 import globalErrorHandler from "./globalErrorHandler";
 import notFoundRoute from "./notFoundRoute";
 
-export const middlewares = {
-	notFoundRoute,
+type Middlewares = {
+	globalErrorHandler: ErrorRequestHandler;
+	notFoundRoute: RequestHandler;
+};
+
+export const middlewares: Middlewares = {
 	globalErrorHandler,
+	notFoundRoute,
 };
